d5a8b01c0df9afc1f3dbab13a7de2aca5e0ff8b6
NV22112-abdullah <160000962+NV22112-abdullah@users.noreply.github.com>
2024-03-03 09:02:32 +0000

    commit css 3 12:19

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,25 @@ import { images } from "../constants";
 
 const Header = () => {
   return <section>
-    <header>
+    <header className="container mx-auto px-5 flex justify-between">
       <div>
        <img src={images.Logo} alt="logo" /> 
       </div>
       <div>
         <ul>
-          <li>
-            <a href="/">
-              
-            </a>
+        <li>
+            <a href="/">Home</a>
+          </li><li>
+            <a href="/">Articles</a>
+          </li><li>
+            <a href="/">Pages</a>
+          </li><li>
+            <a href="/">Pricing</a>
+          </li><li>
+            <a href="/">Faq</a>
           </li>
         </ul>
+        <button>Sign in</button>
       </div>
     </header>
   </section>
