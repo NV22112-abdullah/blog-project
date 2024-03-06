65211e630df925099ed6f94c3bbb7d86ac94bd91
NV22112-abdullah <160000962+NV22112-abdullah@users.noreply.github.com>
2024-03-06 07:13:21 +0000

    3 - creating header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,25 +3,29 @@ import { images } from "../constants";
 
 const Header = () => {
   return <section>
-    <header className="container mx-auto px-5 flex justify-between">
+     <header className="container mx-auto px-5 flex justify-between py-4">
       <div>
        <img src={images.Logo} alt="logo" /> 
       </div>
-      <div>
-        <ul>
-        <li>
+      <div className="flex gap-x-9">
+       <ul className="flex gap-x-5">
+         <li>
             <a href="/">Home</a>
-          </li><li>
+          </li>
+          <li>
             <a href="/">Articles</a>
-          </li><li>
+          </li>
+          <li>
             <a href="/">Pages</a>
-          </li><li>
+          </li>
+          <li>
             <a href="/">Pricing</a>
-          </li><li>
+          </li>
+          <li>
             <a href="/">Faq</a>
           </li>
         </ul>
-        <button>Sign in</button>
+        <button className="border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold">Sign in</button>
       </div>
     </header>
   </section>
