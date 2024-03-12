5ad3b8e22bef9bc6ab9ae6d536eea30e21fc07b8
NV22112-abdullah <160000962+NV22112-abdullah@users.noreply.github.com>
2024-03-12 00:46:39 +0000

    css 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,12 +23,21 @@ return(
 }
 
 const Header = () => {
+const [navIsVisible, setNavIsVisible] = useState(false);
+
+const navVisibilityHandler = () => [
+  setNavIsVisible((curState) => {
+    return!curState;
+  })
+]
+
   return <section>
      <header className="container mx-auto px-5 flex justify-between py-4 items-center">
       <div>
-       <img src={images.Logo} alt="logo" /> 
+       <img className="w-16" src={images.Logo} alt="logo" /> 
       </div>
-      <div className="flex gap-x-9 items-center">
+      <div className={`${navIsVisible ? "right-0" : "-right-full"
+    } flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottem-0 lg:static gap-x-9 items-center`}>
        <ul className="flex gap-x-2 font-semibold">
          {navItemsInfo.map((item) => (
           <Navitem key={item.name} name={item.name} />
